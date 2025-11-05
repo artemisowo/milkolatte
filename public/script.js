21a45d8220053b70dc8cc3f5eac71d5ac3e1d832
Valentina <vperalta22540@gmail.com>
2025-11-05 11:28:50 -0300

    music and audio controls

--- public/script.js
+++ public/script.js
@@ -1,3 +1,50 @@
+// function for index.html
 function back() {
             window.history.back();
         };
+
+// home.html audio control
+
+const music = new Audio()
+const playButton = document.getElementById('play-audio')
+const pauseButton = document.getElementById('pause-audio')
+
+const progressSelector = document.getElementById('progressBar')
+const volumeSelector = document.getElementById('volumeControl')
+
+// autoplays music whenever content loads
+window.onload = () => {
+    music.src = 'music/sacrifice.mp3'
+    music.volume = 0.2
+    music.play()
+}
+
+// make progress bar update when music is playing
+
+music.onloadedmetadata = () => {
+    progressSelector.max = music.duration
+}
+
+progressSelector.onchange = () => {
+    music.currentTime = progressSelector.value
+}
+
+music.ontimeupdate = () => {
+    progressSelector.value = music.currentTime
+}
+
+// updates volume when bar is edited
+
+volumeSelector.onchange = () => {
+    music.volume = volumeSelector.value
+}
+
+// control buttons
+
+playButton.addEventListener('click', () => {
+    music.play()
+});
+
+pauseButton.addEventListener('click', () => {
+    music.pause()
+});
